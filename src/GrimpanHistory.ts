0a3060a572fa8c07cf743094fbfe4c8c51f7dddf
parksangryong <parksangryong@Mac-mini-2.local>
2025-03-28 11:06:46 +0900

    prototype

--- src/GrimpanHistory.ts
+++ src/GrimpanHistory.ts
@@ -2,10 +2,31 @@ import Grimpan from "./AbstractGrimpan.js";
 import IEGrimpan from "./IEGrimpan.js";
 import ChromeGrimpan from "./ChromeGrimpan.js";
 
+interface Cloneable {
+  clone(): Cloneable;
+}
+
+class HistoryStack extends Array implements Cloneable {
+  clone() {
+    return this.slice() as HistoryStack;
+  }
+}
+
 export abstract class GrimpanHistory {
   grimpan: Grimpan;
+  stack: HistoryStack;
+
   protected constructor(grimpan: Grimpan) {
     this.grimpan = grimpan;
+    this.stack = new HistoryStack();
+  }
+
+  getStack() {
+    return this.stack.clone();
+  }
+
+  setStack(stack: HistoryStack) {
+    this.stack = stack.clone();
   }
 
   abstract initialize(): void;
